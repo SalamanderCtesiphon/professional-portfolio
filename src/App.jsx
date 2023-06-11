4e441794b71db796c22b32a7884313eb8d515e1a
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-11 12:32:02 -0500

    added a scroll animation that reveals my work card elements when the element enters the page on scroll

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Spacer from './components/Spacer';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
+
 function App() {
   const [loading, setLoading] = useState(false)
   let color= useState("#ffffff");
