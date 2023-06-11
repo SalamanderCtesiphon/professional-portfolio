c7c0dfb55a6dd6c4d8f20477e74184638b2152ff
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-11 12:11:43 -0500

    created component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div className="container">
+      <h1 className="title">Contact</h1>
+      <p>contact info goes here</p>
+    </div>
+  )
+}
+
+export default Contact
