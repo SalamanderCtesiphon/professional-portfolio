daca89eba70f0e80fee0cf7cba3ae2e6ab5fb5e8
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-11 11:49:52 -0500

    created a hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className="hero">
+      <p className="intro">Hi, my name is</p>
+      <h1 className="title">Shannon Brookshire.</h1>
+      <h1 className="title2">I am a front-end web developer.</h1>
+      <p className="first-paragraph">I am an enthusiastic software engineer specializing in front-end web development. I have a passion for creating beautiful functional user interfaces.</p>
+    </div>
+  )
+}
+
+export default Hero
