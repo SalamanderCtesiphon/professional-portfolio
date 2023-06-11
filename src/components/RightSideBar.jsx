c101dd465a58319941d8f85272786ed92d6f9725
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-11 11:59:15 -0500

    extracted out a component

---
+++ src/components/RightSideBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function RightSideBar() {
+  return (
+    <section className='right-side-bar'>
+      <p><a href="swbrookshire@gmail.com">swbrookshire@gmail.com</a></p>
+      <div id="other-line"></div>
+    </section>
+  )
+}
+
+export default RightSideBar
