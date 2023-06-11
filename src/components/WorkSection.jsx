9c55a84cd32c4da5d181e076332aa3b9926dd816
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-11 12:32:48 -0500

    implemented a scroll animation

--- src/components/WorkSection.jsx
+++ src/components/WorkSection.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 import coffee from '../images/coffee-site.png'
 import fashion from '../images/fashion-boutique.png'
 import resturant from '../images/resturant-page.png'
+import { motion, useAnimation } from "framer-motion";
+
+import { useInView } from "react-intersection-observer";
 
 function WorkSection() {
+
+  const control = useAnimation()
+  const [ref, inView] = useInView()
+
+  const boxVariant = {
+    visible: { opacity: 1, scale: 1},
+    hidden: { opacity: 0, scale: 0 },
+  }
+
+  useEffect(() => {
+      if (inView) {
+        control.start("visible");
+      } else {
+        control.start("hidden")
+      }
+    }, [control, inView]);
+
+    
   return (
-    <div className="container">
+    <motion.div 
+      ref={ref}
+      variants={boxVariant}
+      initial="hidden"
+      animate={control}
+      className="container"
+    >
       <div className="work-card">
         <img src={coffee} alt="my coffee site" />
         <div className="card-info">
@@ -36,7 +63,7 @@ function WorkSection() {
           <p className='intro-paragraph'>This is a full stack fully functional e-commerce site. This is a bunch of other useless infor mation that i am using to style this element.</p>
         </div>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
