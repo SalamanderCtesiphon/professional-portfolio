50e5f35fd22d0a9955998ee4f3bff6554e3441c4
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-11 12:01:24 -0500

    extracted out the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css';
-import logo from './icons/icons8-merge-git-100.png'
-import resumeDownLoad from './assets/ShannonBrookshire.pdf'
 import { useState, useEffect } from 'react'
 import WorkSection from './components/WorkSection';
 import Hero from './components/Hero';
 import LeftSideBar from './components/LeftSideBar';
 import LoadPage from './components/LoadPage';
 import RightSideBar from './components/RightSideBar';
+import Header from './components/Header';
 
 function App() {
   const [loading, setLoading] = useState(false)
@@ -31,17 +30,7 @@ function App() {
           <div>
             <LeftSideBar />
             <RightSideBar />
-            <header>
-              <img id='logo' src={logo} alt="my logo" />
-              <div className="link-container">
-                <div className="link-container-child-one">
-                  <div className="link">About</div>
-                  <div className="link">Work</div>
-                  <div className="link">Contact</div>
-                </div>
-                <div className="resume-link"><a id="sticky" href={resumeDownLoad}>Resume</a></div>
-              </div>
-            </header>
+            <Header />
             <Hero />
             <div className="spacer"></div>
             <WorkSection />           
