34d560270be3b7af742f4220e5a71aac376db694
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-11 12:11:59 -0500

    created component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="footer">
+      <small>this web site is a poor clone of a much better website by a wonderful developer <a className='nav-link' href="https://v4.brittanychiang.com/">Brittany Chaing</a></small>
+    </div>
+)
+}
+
+export default Footer
