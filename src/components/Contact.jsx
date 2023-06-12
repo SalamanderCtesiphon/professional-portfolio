24aa5d6d3585720ba6542c4132a46987b3ec03ec
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-12 14:21:27 -0500

    laying out the contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import { Email } from './Email'
+import twitter from '../icons/icons8-twitter-50.png'
+import linkedIn from '../icons/icons8-linkedin-50.png'
 
 function Contact() {
   return (
     <div className="container">
       <h1 className="title">Contact</h1>
-      <p>contact info goes here</p>
-      <Email />
+      <p>Feel free to contact me either by messaging me on Twitter or LinkedIn or fill out the form below.</p>
+      <div className="contact-container">
+        <div className="link-icon-holder">
+          <li><a href="https://www.linkedin.com/in/shannon-brookshire-817577265/"><img src={linkedIn} alt="linked in" className='icons'/></a></li>
+          <li><a href="https://twitter.com/Shannon99535365"><img src={twitter} alt="twitter" className='icons'/></a></li>
+        
+        </div>
+        <div className="line-box"></div>
+        <Email />
+      </div>
     </div>
   )
 }
