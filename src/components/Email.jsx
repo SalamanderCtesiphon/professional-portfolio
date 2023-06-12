cb4ef62db8302aac01f59c0226345d7359d3ce05
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-12 13:24:10 -0500

    added an id tag to the email form for styling

--- src/components/Email.jsx
+++ src/components/Email.jsx
@@ -23,7 +23,7 @@ export const Email = () => {
       <input type="email" name="user_email" />
       <label>Message</label>
       <textarea name="message" />
-      <input type="submit" value="Send" />
+      <input type="submit" value="Send" id='submit-btn'/>
     </form>
   );
 };
