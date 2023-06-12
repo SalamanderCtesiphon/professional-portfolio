d55dcce78064b73157875e408db14b86fa1ee3d3
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-12 10:39:48 -0500

    added an email form to the contact info

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Email from './Email'
 
 function Contact() {
   return (
     <div className="container">
       <h1 className="title">Contact</h1>
       <p>contact info goes here</p>
+      <Email />
     </div>
   )
 }
