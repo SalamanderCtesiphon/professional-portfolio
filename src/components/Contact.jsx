ee41abebb53bd1dff0d6b8a05f6e1e35b6055c4d
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-12 10:46:59 -0500

    removed the email service

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import Email from './Email'
 
 function Contact() {
   return (
     <div className="container">
       <h1 className="title">Contact</h1>
       <p>contact info goes here</p>
-      <Email />
     </div>
   )
 }
