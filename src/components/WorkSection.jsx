667f48415713d9b798e292afcf240e20db060865
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-12 12:42:58 -0500

    added nav link icons for the live site and for the git hub repos for my projects

--- src/components/WorkSection.jsx
+++ src/components/WorkSection.jsx
@@ -44,8 +44,8 @@ function WorkSection() {
         <div className="card-info">
           <div className="title-holder">
             <h4>Coffee Importers</h4>
-            <a href="#"><img src={next} alt="nav link" className='link-icon'/>live site</a>
-            <a href="#"><img src={next} alt="nav link" className='link-icon'/>github repo</a>
+            <a href="/"><img src={next} alt="nav link" className='link-icon'/>live site</a>
+            <a href="/"><img src={next} alt="nav link" className='link-icon'/>github repo</a>
           </div>
           <p className='intro-paragraph'>This is a full stack fully functional e-commerce site. This is a bunch of other useless infor mation that i am using to style this element.</p>
           <div className="badges">
