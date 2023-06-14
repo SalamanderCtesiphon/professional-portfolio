e0d48b436fee38fd95e983b3dae02ea687a93c9c
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-14 10:46:58 -0500

    fixed a typo in the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className="footer">
-      <small>this web site is a poor clone of a much better website by a wonderful developer <a className='nav-link' href="https://v4.brittanychiang.com/">Brittany Chaing</a></small>
+      <small>this site is a poor clone of a much better website by a wonderful developer <a className='nav-link' href="https://v4.brittanychiang.com/">Brittany Chaing</a></small>
     </div>
 )
 }
