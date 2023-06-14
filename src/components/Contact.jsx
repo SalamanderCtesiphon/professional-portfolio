e8a5380103980ab5c74c8fe65db1ec61640f74c4
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-14 10:12:08 -0500

    styled the contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,12 +7,14 @@ function Contact() {
   return (
     <div className="container">
       <h1 className="title">Contact</h1>
-      <p>Feel free to contact me either by messaging me on Twitter or LinkedIn or fill out the form below.</p>
+      <p>Feel free to contact me either by messaging me on LinkedIn or Twitter... </p>
+      <div className="link-icon-holder">
+        <li><a href="https://www.linkedin.com/in/shannon-brookshire-817577265/"><img src={linkedIn} alt="linked in" className='icons'/></a></li>
+        <li><a href="https://twitter.com/Shannon99535365"><img src={twitter} alt="twitter" className='icons'/></a></li>
+      </div>
+      <p>... or send me an email by filling out the form below.</p>
       <div className="contact-container">
-        <div className="link-icon-holder">
-          <li><a href="https://www.linkedin.com/in/shannon-brookshire-817577265/"><img src={linkedIn} alt="linked in" className='icons'/></a></li>
-          <li><a href="https://twitter.com/Shannon99535365"><img src={twitter} alt="twitter" className='icons'/></a></li>
-        </div>
+        
         <Email />
       </div>
     </div>
